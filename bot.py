450bead8d9f5edf44137ef2e332a7b02c5758f6b
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2025-02-09 02:44:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 import os
 import tempfile
 import subprocess
@@ -27,14 +26,13 @@ def handle_video(client, message):
     if message.animation:
         subprocess.run(f'ffmpeg -y -i "{file}" "{temp_filename}"', shell=True, check=True)
     else:
-            subprocess.run(
-        f'ffmpeg -y -i "{file}" -r {VIDEO_FPS} -c:v {VIDEO_CODEC} -pix_fmt {VIDEO_PIXEL_FORMAT} '
-        f'-b:v {VIDEO_BITRATE} -crf {VIDEO_CRF} -preset {VIDEO_PRESET} -c:a {VIDEO_AUDIO_CODEC} '
-        f'-b:a {VIDEO_AUDIO_BITRATE} -ac {VIDEO_AUDIO_CHANNELS} -ar {VIDEO_AUDIO_SAMPLE_RATE} '
-        f'-profile:v {VIDEO_PROFILE} -map_metadata -1 "{temp_filename}"',
-        shell=True, check=True )
-
-
+        subprocess.run(
+            f'ffmpeg -y -i "{file}" -r {VIDEO_FPS} -c:v {VIDEO_CODEC} -pix_fmt {VIDEO_PIXEL_FORMAT} '
+            f'-b:v {VIDEO_BITRATE} -crf {VIDEO_CRF} -preset {VIDEO_PRESET} -c:a {VIDEO_AUDIO_CODEC} '
+            f'-b:a {VIDEO_AUDIO_BITRATE} -ac {VIDEO_AUDIO_CHANNELS} -ar {VIDEO_AUDIO_SAMPLE_RATE} '
+            f'-profile:v {VIDEO_PROFILE} -map_metadata -1 "{temp_filename}"',
+            shell=True, check=True
+        )
     
     # إرسال الفيديو المضغوط للمستخدم كـ "document" لتسريع عملية الرفع
     message.reply_document(temp_filename, progress=progress)
