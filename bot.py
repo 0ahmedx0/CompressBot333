ba2134cd499c026f5fd99306fad35bcaa93e1b91
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2025-02-09 03:19:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def handle_video(client, message):
     else:
         subprocess.run(
             f'ffmpeg -y -i "{file}" -r {VIDEO_FPS} -c:v {VIDEO_CODEC} -pix_fmt {VIDEO_PIXEL_FORMAT} '
-            f'-b:v {VIDEO_BITRATE} -crf {VIDEO_CRF} -preset {VIDEO_PRESET} -c:a {VIDEO_AUDIO_CODEC} '
+            f'-crf {VIDEO_CRF} -preset {VIDEO_PRESET} -c:a {VIDEO_AUDIO_CODEC} '
             f'-b:a {VIDEO_AUDIO_BITRATE} -ac {VIDEO_AUDIO_CHANNELS} -ar {VIDEO_AUDIO_SAMPLE_RATE} '
             f'-profile:v {VIDEO_PROFILE} -map_metadata -1 "{temp_filename}"',
             shell=True, check=True
